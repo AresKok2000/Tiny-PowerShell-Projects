c56e6503e793e6384d6656283c326b8fc2ff30ca
AresKok2000 <ares.kok@calco.nl>
2026-04-08 15:33:29 +0200

    second assignment

--- 02_crowsnest/crowsnest.ps1
+++ 02_crowsnest/crowsnest.ps1
@@ -1,1 +1,16 @@
-﻿
+﻿param(
+    [string] $Noun
+)
+
+$vowels = @("a", "e", "i", "o", "u")
+
+$lowerNoun = $Noun.ToLower()
+
+if ($Noun.Length -gt 0 -and $vowels -contains $lowerNoun[0]) {
+    $article ="an"
+} else {
+    $article = "a"
+}
+
+$output = "Ahoy, Captain, $article $Noun off the larboard bow!"
+$output
